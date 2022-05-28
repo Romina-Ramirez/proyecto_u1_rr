04ae18be61d4d076170013b9e15c7d276921ca2a
Romina-Ramirez <romina.maite.rt@gmail.com>
2022-05-27 20:05:29 -0500

    Mi primera subida desde consola

---
+++ src/main/java/com/uce/edu/demo/ProyectoU1RrApplication.java
@@ -0,0 +1,21 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU1RrApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU1RrApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto con Spring Framework.");
+		
+	}
+
+}
